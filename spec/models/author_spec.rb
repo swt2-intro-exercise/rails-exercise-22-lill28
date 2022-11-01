dc0069b7017e4e9f93fbf85a885b0acece92089a
Lilly Zintl <Lilly.Zintl@student.hpi.de>
2022-11-01 20:39:22 +0100

    Failing test for #3.

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should save an instance of the author model" do
+    author = Author.new(first_name:"Bilbo", last_name:"Baggins", homepage:"bilbo.baggins@theshire.com")
+    expect(author.first_name).to eq("Bilbo")
+    expect(author.last_name).to eq("Baggins")
+    expect(author.homepage).to eq("bilbo.baggins@theshire.com")
+  end
+  it "should have a name method returning first and last name" do
+    author = Author.new(first_name:"Bilbo", last_name:"Baggins", homepage:"bilbo.baggins@theshire.com")
+    expect(author.name).to eq("Bilbo Baggins")
+  end
+end
