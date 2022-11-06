7661e568e5f9b9edd6cf4b15087dc7fe2aaa4e2a
Lilly Zintl <Lilly.Zintl@student.hpi.de>
2022-11-06 14:58:03 +0100

    Failing test for #10.

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it "should be invalid when any attribute is missing" do
+    paper1 = Paper.new(title:"", venue:"a venue", year:"1234")
+    paper2 = Paper.new(title:"a title", venue:"", year:"1234")
+    paper3 = Paper.new(title:"a title", venue:"a venue", year:"")
+    paper4 = Paper.new(title:"a title", venue:"a venue", year:"hululululu")
+    expect(paper1).to_not be_valid
+    expect(paper2).to_not be_valid
+    expect(paper3).to_not be_valid
+    expect(paper4).to_not be_valid
+  end
 end
