d8c53983e9e77226707e28742e8e7c485bb806bb
Lilly Zintl <Lilly.Zintl@student.hpi.de>
2022-11-06 10:46:34 +0100

    Failing test for #4.

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -3,6 +3,18 @@ require 'rails_helper'
 describe "render index author page", type: :feature do
     it "should exist at 'author_path' and render withour error" do
         # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
-        visit authors_url
+        visit authors_path
+    end
+end
+
+describe "contents author index page", type: :feature do
+    it "should display a table" do
+        visit authors_path
+        expect(page).to have_table '#all_authors'
+    end
+    it "should have a header with Name and Homepage" do
+        visit authors_path
+        find('th').text.should include('Name')
+        find('th').text.should include('Homepage')
     end
 end
