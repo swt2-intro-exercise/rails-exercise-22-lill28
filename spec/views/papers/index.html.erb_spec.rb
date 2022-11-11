8cd636e069abf3a1bee51f773abf1725e7169d4c
Lilly Zintl <Lilly.Zintl@student.hpi.de>
2022-11-11 09:53:43 +0100

    Failing test for #11.

--- spec/views/papers/index.html.erb_spec.rb
+++ spec/views/papers/index.html.erb_spec.rb
@@ -16,11 +16,17 @@ RSpec.describe "papers/index", type: :view do
     ])
   end
 
+  puts :papers
+
   it "renders a list of papers" do
     render
     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
     assert_select cell_selector, text: Regexp.new("Title".to_s), count: 2
     assert_select cell_selector, text: Regexp.new("Venue".to_s), count: 2
     assert_select cell_selector, text: Regexp.new(2.to_s), count: 2
+    expect(cell_selector).to  have_link 'Edit Paper', href: edit_paper_path(0)
+    expect(cell_selector).to  have_link 'Edit Paper', href: edit_paper_path(1)
+
+
   end
 end
